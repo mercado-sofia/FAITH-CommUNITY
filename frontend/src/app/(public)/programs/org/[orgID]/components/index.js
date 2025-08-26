c0dacf439b32e4515229208169f4518a05f69467
mercado-sofia <sofia1809.mercado@gmail.com>
2025-08-26 23:04:20 +0800

    UI Updates

---
+++ frontend/src/app/(public)/programs/org/[orgID]/components/index.js
@@ -0,0 +1,4 @@
+export { default as OrgInfoCard } from './OrgInfoCard';
+export { default as AdvocacyCompetency } from './AdvocacyCompetency';
+export { default as FeaturedProjects } from './Projects';
+export { default as OrgHeadsCarousel } from './OrgHeadsCarousel';
